a1fae63d11be5d12a57e3cf4192de5edf58bc821
surfqce <87330578+surfqce@users.noreply.github.com>
2021-07-19 12:59:10 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var weight= [35,38,40,48]
+
+function setup() 
+{
+  createCanvas(400,400);
+
+  var sum=weight[0]+weight[1]+ weight[2]+ weight[3]
+  var average=sum/weight.length;
+  console.log (average)
+}
+
+function draw() 
+{
+background(51);
+
+}
+
